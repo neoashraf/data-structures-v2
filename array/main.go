fb1de0ca88cb9cbe140dc998cf424a0a3ea8bc9b
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2019-01-19 14:09:10 +0600

    array rotation - done with JugglingRotation
    >>> Why is this Change Necessary?:
    
    >>> This change address the need by:
    
    >>> What Side Effects does this change have?
    
    >>> Issue #None

--- array/main.go
+++ array/main.go
@@ -3,15 +3,22 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 
 	"github.com/alamin-mahamud/go-data-structures/array/rotation"
 )
 
 func main() {
-	// rotation.SimpleRotation([]int{1, 2, 3, 4, 5, 6}, 3)
 	d := flag.Int("d", 2, "rotate by this value")
 	flag.Parse()
-	a := []int{1, 2, 3, 4, 5, 6}
-	rotation.RotateOneByOne(&a, *d)
+	a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14}
+
+	if *d > len(a) {
+		os.Exit(1)
+	}
+
+	// rotation.SimpleRotation([]int{1, 2, 3, 4, 5, 6}, 3)
+	// rotation.RotateOneByOne(&a, *d)
+	rotation.JugglingRotation(&a, *d)
 	fmt.Println(a)
 }
